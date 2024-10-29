c98ec938b0079243c5292ad7da6fb2f297188b80
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-10-29 13:33:33 +0300

    Add files via upload

---
+++ even numbers in the specified range/even numbers in the specified range/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        if (int.TryParse(Console.ReadLine(), out int num1))
+        {
+            Console.WriteLine("Введите второе число:");
+            if (int.TryParse(Console.ReadLine(), out int num2))
+            {
+                if (num1 > num2)
+                {
+                    int temp = num1;
+                    num1 = num2;
+                    num2 = temp;
+                }
+
+                Console.WriteLine("Четные числа в диапазоне:");
+                for (int i = num1; i <= num2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введено некорректное второе число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: Введено некорректное первое число.");
+        }
+    }
+}
